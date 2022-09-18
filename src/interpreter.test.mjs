0b78522de260683229e716d226919337242f7cc9
James Wright <hi@james.engineering>
2022-09-18 17:24:54 +0100

    Updates result type in test

--- src/interpreter.test.mjs
+++ src/interpreter.test.mjs
@@ -3,7 +3,7 @@ import run from "./interpreter.mjs";
 describe("Kibi interpreter end-to-end tests", () => {
   describe("Simple program", () => {
     it("should return the expected result", () => {
-      expect(run("(+ 2 2 2)")).toBe("6");
+      expect(run("(+ 2 2 2)")).toBe(6);
     });
   });
 });
